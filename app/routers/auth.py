7dafe6408650677b1dcb92cf19a90d5c7233eaa3
thesnac <thesna9@gmail.com>
2026-07-30 15:59:52 +0530

    auth page updated

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -69,19 +69,19 @@ def register(user: RegisterUser, db: Session = Depends(get_db)):
             User.user_id == user.enroller_id
         ).first()
 
-        if not enroller:
-            raise HTTPException(
-                status_code=status.HTTP_400_BAD_REQUEST,
-                detail="Invalid Enroller ID"
-            )
+        # if not enroller:
+        #     raise HTTPException(
+        #         status_code=status.HTTP_400_BAD_REQUEST,
+        #         detail="Invalid Enroller ID"
+        #     )
 
     # Find placement parent
-    placement_parent = find_placement_parent(
-        db=db,
-        sponsor=enroller,
-        club=user.club
-    )
-    # placement_parent = ""
+    # placement_parent = find_placement_parent(
+    #     db=db,
+    #     sponsor=enroller,
+    #     club=user.club
+    # )
+    placement_parent = ""
 
         # if enroller.role != "ADMIN":
         #     raise HTTPException(
