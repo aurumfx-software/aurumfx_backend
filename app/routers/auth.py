0da69ee1181a9c652c7f77ac5a907246251d619a
thesnac <thesna9@gmail.com>
2026-07-30 16:48:32 +0530

    auth update for debug reg

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -34,9 +34,7 @@ def register(user: RegisterUser, db: Session = Depends(get_db)):
             status_code=status.HTTP_400_BAD_REQUEST,
             detail="Password mismatch"
         )
-    print("Password length:", len(user.password.encode("utf-8")))
-    print("User ID:", user.user_id)
-
+    
     # Email already exists
     existing_email = db.query(User).filter(
         User.email == user.email
@@ -71,19 +69,19 @@ def register(user: RegisterUser, db: Session = Depends(get_db)):
             User.user_id == user.enroller_id
         ).first()
 
-        # if not enroller:
-        #     raise HTTPException(
-        #         status_code=status.HTTP_400_BAD_REQUEST,
-        #         detail="Invalid Enroller ID"
-        #     )
+        if not enroller:
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail="Invalid Enroller ID"
+            )
 
     # Find placement parent
-    # placement_parent = find_placement_parent(
-    #     db=db,
-    #     sponsor=enroller,
-    #     club=user.club
-    # )
-    placement_parent = ""
+    placement_parent = find_placement_parent(
+        db=db,
+        sponsor=enroller,
+        club=user.club
+    )
+    # placement_parent = ""
 
         # if enroller.role != "ADMIN":
         #     raise HTTPException(
