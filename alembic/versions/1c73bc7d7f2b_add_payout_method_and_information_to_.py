86d9a2b236287059af42873587346d9bff4b6acf
thesnac <thesna9@gmail.com>
2026-08-16 10:10:15 +0530

    Restore payout migration parent revision

--- alembic/versions/1c73bc7d7f2b_add_payout_method_and_information_to_.py
+++ alembic/versions/1c73bc7d7f2b_add_payout_method_and_information_to_.py
@@ -13,7 +13,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision: str = '1c73bc7d7f2b'
-down_revision: Union[str, Sequence[str], None] = 'e26d8fa59045'
+down_revision: Union[str, Sequence[str], None] = '52e3d94f3db7'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
